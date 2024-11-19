3dc56067f6e85a89ac6a0939ff259555775d8b67
AndriiHliuza <gl093770@gmail.com>
2024-11-19 22:38:36 +0200

    Refactoring

--- src/main/java/org/pzks/parsers/math/laws/CommutativePropertyBasedSyntaxUnitProcessor.java
+++ src/main/java/org/pzks/parsers/math/laws/CommutativePropertyBasedSyntaxUnitProcessor.java
@@ -9,7 +9,6 @@ import org.pzks.units.*;
 import org.pzks.units.Number;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class CommutativePropertyBasedSyntaxUnitProcessor {
